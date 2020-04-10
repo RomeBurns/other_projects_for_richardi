af3d0a032a9d036f3e4739c4b6bee67ad54d9ed2
Julian Brochu <nonspamerjbb@gmail.com>
2020-04-10 14:58:40 +0000

    d

---
+++ python_book/3.3exercises.py
@@ -0,0 +1,24 @@
+#the domain of this function is all real, x != 2. the range is all real
+
+#domain: x >= -1 of all real. the range is all real.
+
+#domain: -1 <= x <= 1 of all real, range: real -1 <= f(x) <= 1
+
+# the domain has 899 elements, the range has 27
+
+def problem5(s):
+    s = int(s)
+    if s > 0:
+        return ((s * 2) - 1)
+
+#a plane is two dimensional, so coordinayes would be used. as zero is within all sets, it should be the center of the plane and the one value that is constant between the two sets
+def problem6(a, b):
+    x = 4 * a
+    y = 3 * b
+    return x, y
+
+#3
+
+#sqrt(1-x^2)
+
+#1 and -2, respectively
