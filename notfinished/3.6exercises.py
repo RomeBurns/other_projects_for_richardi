793def9bce1a8da996527dc95b394b6c13725db3
Julian Brochu <nonspamerjbb@gmail.com>
2020-04-24 14:16:41 +0000

    hope this is good

--- notfinished/3.6exercises.py
+++
@@ -1,7 +0,0 @@
-# min will produce zero, max will produce n - 1
-
-def num_digits(s):
-    return (len(str(s)))
-
-def problem3(s):
-    return (sum(int(str(s))))
---
+++ python_book/chapter3/3.6exercises.py
@@ -0,0 +1,16 @@
+# min will produce zero, max will produce n - 1
+
+def num_digits(s):
+    return (len(str(s)))
+
+def problem3(s):
+    return (sum(int(i) for i in str(s)))
+
+# an error is created, as list is unhashable
+# this works and returns 1, as the tuple is one element
+
+# 23 is the result, as they are strings yet to be converted to integers.
+
+# 2 to the 8th power modulus 5, which yields 1
+
+# it will produce the set of numerically sorted square roots of 1 through 19 rounded to the nearest whole number.
